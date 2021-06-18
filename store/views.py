26e5ba00497f736f34f3f3957b06f2be2c60d587
Niranjan4712 <niranjan.patil.8888@gmail.com>
2021-06-18 17:58:30 +0530

    first commit

---
+++ store/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+def store(request):
+     context = {} 
+     return render(request, 'store/store.html', context)
+
+def cart(request):
+     context = {} 
+     return render(request, 'store/cart.html', context)
+     
+def checkout(request):
+     context = {} 
+     return render(request, 'store/checkout.html', context)
+
+def ContactUs(request):
+    if request.method=="POST":
+        cont=Contact() #HERE I MADE OBJECT
+        name=request.POST.get('name') #we have taken this name from input name contact html page 
+        email=request.POST.get('email')
+        contact=request.POST.get('contact')
+        query=request.POST.get('query')
+         #here we have taken contact_.name=name the first name attribute is from models and the seocnd name attribute from this page means viwes(app)
+        cont.name=name
+        cont.email=email
+        
+        cont.contact=contact
+        cont.query=query=query
+        cont.save()
+        messages.success(request,'Your message has been sent!')
+    return render(request,'store/ContactUs.html')
+
+def AboutUs(request):
+    context={}
+    return render(request,'store/AboutUs.html',context)
+
+def Account(request):    
+    return render(request,'store/Account.html')
+
+
